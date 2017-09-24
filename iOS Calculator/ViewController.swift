c26bc3b27f9af6bf6a9ceb88ad6dadb9a2893a6b
Victoria Leaf <victorialeaf@Victorias-MacBook-Air.local>
2017-09-24 10:54:09 -0500

    Initial Commit

---
+++ iOS Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Calculator
+//
+//  Created by Victoria Leaf on 9/24/17.
+//  Copyright © 2017 Victoria Leaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
